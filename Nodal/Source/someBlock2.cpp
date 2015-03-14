68d8e46a9b7c6c7d4beeb4e1c6bbb0bafce06fee
chris.hoen <chris.hoen@verizon.net>
2015-03-13 21:22:32 -0400

    getting there

--- Nodal/Source/someBlock2.cpp
+++ Nodal/Source/someBlock2.cpp
@@ -52,7 +52,7 @@ void Block2B::initializeBlockUniverse(
    Block2B::mBlockUniverse.initialize(
       aAllocateShortTermBlocks,
       aAllocateLongTermBlocks,
-      sizeof(Block2A));
+      sizeof(Block2B));
 }
 
 //******************************************************************************
@@ -149,26 +149,6 @@ Block2A::~Block2A()
 //******************************************************************************
 //******************************************************************************
 //******************************************************************************
-// Constructor, it is called by create after allocation of a new block.
-
-Block2B::Block2B()
-{
-   Prn::print(0, 0, "Block2B::Block2B");
-}
-
-//******************************************************************************
-//******************************************************************************
-//******************************************************************************
-// Destructor, it is called by destroy before deallocation of a block.
-
-Block2B::~Block2B()
-{
-   Prn::print(0, 0, "Block2B::~Block2B");
-}
-
-//******************************************************************************
-//******************************************************************************
-//******************************************************************************
 // This static method allocates a block from system memory or from block
 // universe short term or long term memory block pools. After allocation,
 // it calls the class constructor on the allocated block. The memory type
@@ -177,13 +157,30 @@ Block2B::~Block2B()
 
 Block2B* Block2B::create(int aMemoryType)
 {
-   // Allocate a block from the block pool
-   Block2B* tPointer = (Block2B*)Block2B::mBlockUniverse.mLongTermBlockPool.get();
+   // Block pointer
+   Block2B* tPointer = 0;
+
+   // Allocate memory for the block
+   switch (aMemoryType)
+   {
+   case CC::MemoryType_System :
+      // Allocate a block from system memory
+      tPointer = (Block2B*)malloc(sizeof(Block2B));
+      break;
+   case CC::MemoryType_ShortTerm :
+      // Allocate a block from the short term block pool
+      tPointer = (Block2B*)Block2B::mBlockUniverse.mShortTermBlockPool.get();
+      break;
+   case CC::MemoryType_LongTerm :
+      // Allocate a block from the long term block pool
+      tPointer = (Block2B*)Block2B::mBlockUniverse.mLongTermBlockPool.get();
+      break;
+   }
 
    // Call the constructor on the allocated block using placement new
    tPointer = new(tPointer)Block2B;
 
-   // Store memory type
+   // Store block memory type
    tPointer->mMemoryType = aMemoryType;
 
    // Return the allocated block
@@ -202,9 +199,43 @@ void Block2B::destroy()
    // Call the block's destructor
    this->~Block2B();
 
-   // Deallocate the block back to the block pool
-   Block2B::mBlockUniverse.mLongTermBlockPool.put(this);
+   // Deallocate memory for the block
+   switch (this->mMemoryType)
+   {
+   case CC::MemoryType_System :
+      // Deallocate the block back to system memory
+      free(this);
+      break;
+   case CC::MemoryType_ShortTerm :
+      // Deallocate the block back to the short term block pool
+      break;
+   case CC::MemoryType_LongTerm :
+      // Deallocate the block back to the long term block pool
+      Block2B::mBlockUniverse.mLongTermBlockPool.put(this);
+      break;
+   }
 }
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Constructor, it is called by create after allocation of a new block.
+
+Block2B::Block2B()
+{
+   Prn::print(0, 0, "Block2B::Block2B");
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Destructor, it is called by destroy before deallocation of a block.
+
+Block2B::~Block2B()
+{
+   Prn::print(0, 0, "Block2B::~Block2B");
+}
+
 
    
 }//namespace
