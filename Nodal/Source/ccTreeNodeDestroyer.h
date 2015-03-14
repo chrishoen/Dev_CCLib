8350e307276bafbec3e589fb337f87045297d06d
chris.hoen <chris.hoen@verizon.net>
2015-03-14 01:43:40 -0400

    comments

--- Nodal/Source/ccTreeNodeDestroyer.h
+++ Nodal/Source/ccTreeNodeDestroyer.h
@@ -7,9 +7,19 @@
 
 namespace CC
 {
+   // This destroys a collection of tree nodes, starting at and including a 
+   // subject root node. It recursively traverses the nodes in destruction
+   // order, such that child nodes are destroyed before parent nodes. It
+   // recursively destroys all of the nodes below the subject root node
+   // and then destroys the subject root node.
 
    void destroyAllTreeNodes(
       TreeNode*        aSubjectNode);
+
+   // This destroys all child nodes below a subject node. It is called 
+   // by the above recursive destroy function. It recursively traverses 
+   // the nodes in destruction order, such that child nodes are 
+   // destroyed before parent nodes.
 
    void destroyAllTreeNodesBelow(
       TreeNode*        aSubjectNode,
