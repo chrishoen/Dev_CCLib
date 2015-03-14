8152fc8e3a6b322be6ff0617582e9f20ceddfef6
chris.hoen <chris.hoen@verizon.net>
2015-03-13 23:36:58 -0400

    looks good with the memory

--- Nodal/Source/ccLabelledTreeNodeVisitor.cpp
+++ Nodal/Source/ccLabelledTreeNodeVisitor.cpp
@@ -66,14 +66,14 @@ void printAllLabelledTreeNodes3(LabelledTreeNode* aSubjectNode)
 void visitLabelledTreeNodeForDestroy (TreeNode* aSubjectNode, RecursiveAnchor* aRecursiveAnchor)
 {
    LabelledTreeNode* tSubjectNode = static_cast<LabelledTreeNode*>(aSubjectNode);
-
+#if 0
    printf("visitLabelledTreeNodeForDestroy %3d %1d %1d %5d %s\n", 
       aRecursiveAnchor->mIndex, 
       aRecursiveAnchor->mFirstInLevel,
       aRecursiveAnchor->mLevel,
       tSubjectNode->mIdentifier,
       tSubjectNode->mFullPath);
-
+#endif
    tSubjectNode->destroy();
 }
 
