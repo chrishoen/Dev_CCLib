d77b6b8293189c514d206b2cfd6f2d059d61ba6c
chris.hoen <chris.hoen@verizon.net>
2015-03-27 00:21:46 -0400

    comments

--- Nodal/Source/ccTreeNodeRxQueue.h
+++ Nodal/Source/ccTreeNodeRxQueue.h
@@ -21,6 +21,9 @@ namespace CC
 // transmitted over a communications channel. At the receiving end of the
 // channel the individual blocks are put to a tree node receive queue,
 // where the tree node structure is reconstructed.
+//
+// A structure will not be released from a queue until all of its nodes
+// have been received.
 
 template <class TreeNodeClass>
 class TreeNodeRxQueue
@@ -56,48 +59,76 @@ public:
       mParentAtLevel[0] = mRootNode;
    }
 
+//****************************************************************************
+//****************************************************************************
+//****************************************************************************
+// Put a node that was received from a communications channel to the queue.
+
    void putRxNode(TreeNodeClass* aNode)
    {
-
+      // If the queue is empty
       if (mRootNode->mFirstChildNode==0)
       {
+         // Attach the node to the root
          mRootNode->attachAfterLastChild(aNode);
+         // Set the node level to point to the root
          aNode->mTxAttachLevel = 1;
+         // Store the node
          mPutNode = aNode;
       }
       else
       {
+         // If the node level is greater than the previous node level
+         // then it is a first child of the previous node
          if (aNode->mTxAttachLevel > mPutNode->mTxAttachLevel)
          {
+            // Store the previous node in the parent node array
             mParentAtLevel[aNode->mTxAttachLevel - 1] = mPutNode;
          }
+
+         // Attach the node as the last child of the parent node that is 
+         // indexed by the node's attachment level
          mParentAtLevel[aNode->mTxAttachLevel - 1]->attachAfterLastChild(aNode);
+
+         // Store the node
          mPutNode = aNode;
+
+         // Store the node's parent in the parent node array
          mParentAtLevel[aNode->mTxAttachLevel] = mPutNode->mParentNode;
       }
+      // If the node is the last one of a structure that was transitted
       if (aNode->mTreeNodeTxFlags.mIsLastInStructure)
       {
+         // Set the last in structure flag for the last child node of the root.
+         // A structure will not be released from the queue until all nodes
+         // of the structure have been received.
          mRootNode->mLastChildNode->mTreeNodeTxFlags.mIsLastInStructure = true;
       }
    }
 
+//****************************************************************************
+//****************************************************************************
+//****************************************************************************
+// Get the root node of a node structure that was received. A structure will
+// not be released from a queue until all of its nodes have been received.
+
    TreeNodeClass* getNextRxNode()
    {
+      // If the queue is empty return zero
       if (mRootNode->mFirstChildNode == 0)
       {
          return 0;
       }
 
+      // If the first child of the root has not received all of its nodes
+      // then return zero
       if (!mRootNode->mFirstChildNode->mTreeNodeTxFlags.mIsLastInStructure)
       {
          return 0;
       }
 
-      TreeNodeClass* tNode = (TreeNodeClass*)mRootNode->detachFirstChild();
-      if (tNode == 0)
-      {
-      }
-      return tNode;
+      // Else detach the first child from the root and return it
+      return (TreeNodeClass*)mRootNode->detachFirstChild();
    }
 };
 
