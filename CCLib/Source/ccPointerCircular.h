b2b0ebdbd995370896ea1b7dd7622e40f45170b8
chris.hoen <chris.hoen@verizon.net>
2015-04-24 13:24:18 -0400

    comments

--- CCLib/Source/ccPointerCircular.h
+++ CCLib/Source/ccPointerCircular.h
@@ -19,7 +19,8 @@ namespace CC
 // This class encapsulates a circular array of pointers. It maintains an array
 // of pointers that are dynamically allocated at initialization. Access to the
 // array is done is a circular manner. It is used to implement circular
-// buffers.
+// buffers. This is used by the short term memory pool classes to maintain 
+// pointers into arrays of allocated memory blocks.
 
 class PointerCircular
 {
@@ -35,10 +36,15 @@ public:
    // aAllocate is the number of pointers to allocate, the size of the array.
    void initialize(int aAllocate);
 
-   // Put a pointer into the circular array.
+   // Put a pointer into the circular array. This is used during initialization
+   // of short term memory pools by filling up the pointer array with allocated
+   // block addresses. 
    bool  put(void* aPointer);
 
-   // Get a pointer from the circular array.
+   // Get a pointer from the circular array. This is used to allocate a block
+   // from a short term memory pool. It returns the address of an allocated
+   // block. It does so in a circular manner by indexing through the pointer
+   // array.
    void* get();
 
    //---------------------------------------------------------------------------
@@ -47,10 +53,12 @@ public:
    // Array of pointers, Dynamically allocated by initialize.
    void** mArray;
 
-   // Index into the array, to put to.
+   // Index into the array. Put operations cycle through the array via this
+   // index.
    int mPutIndex;
 
-   // Index into the array, to get from.
+   // Index into the array. Get operations cycle through the array via this
+   // index.
    int mGetIndex;
 
    // Size of the array, number of pointers allocated.
