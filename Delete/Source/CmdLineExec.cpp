9e2316df5491259a4950fd65709647eaa37ed26d
chris.hoen <chris.hoen@verizon.net>
2015-05-08 01:04:31 -0400

    added Delete project

---
+++ Delete/Source/CmdLineExec.cpp
@@ -0,0 +1,61 @@
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "prnPrint.h"
+#include "someClass1.h"
+
+#include "CmdLineExec.h"
+
+//******************************************************************************
+CmdLineExec::CmdLineExec()
+{
+}
+//******************************************************************************
+void CmdLineExec::reset()
+{
+}
+//******************************************************************************
+void CmdLineExec::execute(Ris::CmdLineCmd* aCmd)
+{
+   if(aCmd->isCmd("RESET"  ))  reset();
+   if(aCmd->isCmd("GO1"    ))  executeGo1(aCmd);
+   if(aCmd->isCmd("GO2"    ))  executeGo2(aCmd);
+   if(aCmd->isCmd("GO3"    ))  executeGo3(aCmd);
+   if(aCmd->isCmd("GO4"    ))  executeGo4(aCmd);
+   if(aCmd->isCmd("GO5"    ))  executeGo5(aCmd);
+}
+
+//******************************************************************************
+
+void CmdLineExec::executeGo1(Ris::CmdLineCmd* aCmd)
+{
+   Some::Class1A* tX = new Some::Class1A(101,102);
+   delete tX;
+}
+
+//******************************************************************************
+
+void CmdLineExec::executeGo2(Ris::CmdLineCmd* aCmd)
+{
+}
+
+//******************************************************************************
+
+void CmdLineExec::executeGo3(Ris::CmdLineCmd* aCmd)
+{
+}
+
+//******************************************************************************
+
+void CmdLineExec::executeGo4(Ris::CmdLineCmd* aCmd)
+{
+}
+
+//******************************************************************************
+
+void CmdLineExec::executeGo5(Ris::CmdLineCmd* aCmd)
+{
+}
+
---
+++ Delete/Source/Main.cpp
@@ -0,0 +1,44 @@
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "prnPrint.h"
+#include "risCmdLineConsole.h"
+#include "CmdLineExec.h"
+
+void main_initialize();
+
+
+//******************************************************************************
+int main(int argc,char** argv)
+{
+   //--------------------------------------------------------------------
+   // Begin program
+
+   main_initialize();
+
+   Prn::print(0,0,"amain***************************************************BEGIN");
+   
+   //--------------------------------------------------------------------
+   // Start user command line executive,
+   // Wait for user to exit
+
+   CmdLineExec* exec = new CmdLineExec;
+   Ris::gCmdLineConsole.execute(exec);
+   delete exec;
+
+   //--------------------------------------------------------------------
+   // End program
+
+   Prn::print(0,0,"amain*****************************************************END");
+   return 0;
+}
+//******************************************************************************
+void main_initialize()
+{
+   // Initialize print filters
+   Prn::initializePrint();
+
+   Ris::setConsoleTitle("test");
+}
+
