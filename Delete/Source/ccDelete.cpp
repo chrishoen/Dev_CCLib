5274b70a474a1128d4686db9b8dcd9661e4b3485
chris.hoen <chris.hoen@verizon.net>
2015-05-08 01:53:30 -0400

    comments

--- Delete/Source/ccDelete.cpp
+++ Delete/Source/ccDelete.cpp
@@ -6,15 +6,37 @@ Description:
 //******************************************************************************
 //******************************************************************************
 
+/*
+This a global replacement for the delete operator. If this .cpp file is 
+included in a build then the following delete operator overrides all deletes
+for the entire program. The following delete operation actually replaces all
+delete calls made anywhere in the program. This is in accordance with the cpp
+language definition. This replacement is implemented at link time.
+
+I want to repeat: If you include this .cpp file in your build then the
+following delete operator will replace the default delete operator. It 
+will be called by any calls to delete anywhere in your program.
+
+Including this .cpp file in a build is quite frightening.
+
+Here's where I found out about it:
+
+http://en.cppreference.com/w/cpp/memory/new/operator_delete
+
+The versions (1-8) are implicitly declared in each translation unit even if the
+<new> header is not included. These functions are replaceable: a user-provided
+non-member function with the same signature defined anywhere in the program, 
+in any source file, replaces the corresponding implicit version for the entire 
+program. Its declaration does not need to be visible. 
+
+The behavior is undefined if more than one replacement is provided in the
+program or if a replacement is defined with the inline specifier, the program 
+is ill-formed if a replacement is defined in namespace other than global 
+namespace, or if it is defined as a static non-member function at global scope
+
+*/
 #include <cstdio>
 #include <cstdlib>
-#if 0
-// replacement of a minimal set of functions:
-void* operator new(std::size_t sz) {
-    std::printf("global op new called, size = %zu\n",sz);
-    return std::malloc(sz);
-}
-#endif
 
 void operator delete(void* ptr)
 {
