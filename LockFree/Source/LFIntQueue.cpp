8bcd3261b39ebb0616538c32e10d1aab4cc3c844
chris.hoen <chris.hoen@verizon.net>
2016-02-02 21:27:02 -0500

    back to friday

--- LockFree/Source/LFIntQueue.cpp
+++ LockFree/Source/LFIntQueue.cpp
@@ -39,8 +39,8 @@ namespace LFIntQueue
    //***************************************************************************
    // Free List Members
 
-   bool listPop(int* aIndex);
    bool listPush(int  aIndex);
+   bool listPop(int* aIndex);
 
    atomic<int>   mListSize;
    AtomicLFIndex mListHead;
@@ -68,16 +68,8 @@ namespace LFIntQueue
 
    atomic<unsigned long long> mWriteRetry;
    atomic<unsigned long long> mReadRetry;
-
    atomic<unsigned long long> mPopRetry;
-   atomic<unsigned long long> mPopRetry1;
-   atomic<unsigned long long> mPopRetry2;
-   atomic<unsigned long long> mPopRetry3;
-
    atomic<unsigned long long> mPushRetry;
-   atomic<unsigned long long> mPushRetry1;
-   atomic<unsigned long long> mPushRetry2;
-   atomic<unsigned long long> mPushRetry3;
 
    unsigned long long writeRetry() {return mWriteRetry.load();}
    unsigned long long readRetry()  {return mReadRetry.load();}
@@ -119,14 +111,8 @@ namespace LFIntQueue
 
       mWriteRetry = 0;
       mReadRetry  = 0;
-      mPopRetry   = 0;
-      mPopRetry1   = 0;
-      mPopRetry2   = 0;
-      mPopRetry3   = 0;
       mPushRetry  = 0;
-      mPushRetry1  = 0;
-      mPushRetry2  = 0;
-      mPushRetry3  = 0;
+      mPopRetry   = 0;
 }
 
    //***************************************************************************
@@ -154,20 +140,11 @@ namespace LFIntQueue
    void show()
    {
       char tString[40];
-      Prn::print(0,"LFIntQueue---------------------------\n");
+      Prn::print(0,"LFIntQueue\n");
       Prn::print(0,"WriteRetry         %16s",my_stringLLU(tString,mWriteRetry));
       Prn::print(0,"ReadRetry          %16s",my_stringLLU(tString,mReadRetry));
-      Prn::print(0,"");
       Prn::print(0,"PopRetry           %16s",my_stringLLU(tString,mPopRetry));
-      Prn::print(0,"PopRetry1          %16s",my_stringLLU(tString,mPopRetry1));
-      Prn::print(0,"PopRetry2          %16s",my_stringLLU(tString,mPopRetry2));
-      Prn::print(0,"PopRetry3          %16s",my_stringLLU(tString,mPopRetry3));
-      Prn::print(0,"");
       Prn::print(0,"PushRetry          %16s",my_stringLLU(tString,mPushRetry));
-      Prn::print(0,"PushRetry1         %16s",my_stringLLU(tString,mPushRetry1));
-      Prn::print(0,"PushRetry2         %16s",my_stringLLU(tString,mPushRetry2));
-      Prn::print(0,"PushRetry3         %16s",my_stringLLU(tString,mPushRetry3));
-      Prn::print(0,"");
    }
 
    //***************************************************************************
@@ -262,46 +239,6 @@ namespace LFIntQueue
       return true;
    }
 
-   //******************************************************************************
-   //******************************************************************************
-   //******************************************************************************
-   // This detaches the head node.
-
-   bool listPop(int* aNode)
-   {
-      LFIndex tHead;
-
-      int tLoopCount=0;
-      while (true)
-      {
-         // Store the head node in a temp.
-         // This is the node that will be detached.
-         tHead = mListHead.load();
-
-         // Exit if the list is empty.
-         if (tHead.mIndex == cInvalid) return false;
-
-         // Set the head node to be the node that is after the head node.
-         if (mListHead.compare_exchange_weak(tHead, LFIndex(mNode[tHead.mIndex].mListNext.load().mIndex,tHead.mCount+1))) break;
-
-         if (++tLoopCount==10000) throw 103;
-      }
-      if (tLoopCount != 0)
-      {
-         mPopRetry++;
-         if (tLoopCount == 1) mPopRetry1++;
-         else if (tLoopCount == 2) mPopRetry2++;
-         else if (tLoopCount == 3) mPopRetry3++;
-      }
-
-      // Return the detached original head node.
-      *aNode = tHead.mIndex;
-
-      // Done.
-      mListSize--;
-      return true;
-   }
-
    //***************************************************************************
    //***************************************************************************
    //***************************************************************************
@@ -314,6 +251,8 @@ namespace LFIntQueue
       int tLoopCount=0;
       while (true)
       {
+         if (++tLoopCount == 10000) throw 103;
+
          // Store the head node in a temp.
          tHead = mListHead.load();
 
@@ -322,19 +261,45 @@ namespace LFIntQueue
 
          // The pushed node is the new head node.
          if (mListHeadIndexRef.compare_exchange_weak(tHead.mIndex, aNode)) break;
-
-         if (++tLoopCount == 10000) throw 103;
-      }
-      if (tLoopCount != 0)
-      {
          mPushRetry++;
-         if (tLoopCount == 1) mPushRetry1++;
-         else if (tLoopCount == 2) mPushRetry2++;
-         else if (tLoopCount == 3) mPushRetry3++;
       }
 
       // Done.
       mListSize++;
+      return true;
+   }
+
+   //******************************************************************************
+   //******************************************************************************
+   //******************************************************************************
+   // This detaches the head node.
+
+   bool listPop(int* aNode)
+   {
+      LFIndex tHead;
+
+      int tLoopCount=0;
+      while (true)
+      {
+         if (++tLoopCount==10000) throw 104;
+
+         // Store the head node in a temp.
+         // This is the node that will be detached.
+         tHead = mListHead.load();
+
+         // Exit if the list is empty.
+         if (tHead.mIndex == cInvalid) return false;
+
+         // Set the head node to be the node that is after the head node.
+         if (mListHead.compare_exchange_weak(tHead, LFIndex(mNode[tHead.mIndex].mListNext.load().mIndex,tHead.mCount+1))) break;
+         mPopRetry++;
+      }
+
+      // Return the detached original head node.
+      *aNode = tHead.mIndex;
+
+      // Done.
+      mListSize--;
       return true;
    }
 
