8190065cd6f330a6406a1b8d3ff84b936e69ecef
chris.hoen <chris.hoen@verizon.net>
2016-03-03 15:25:52 -0500

    comments

--- Containers/Source/ccLFIndex.h
+++ Containers/Source/ccLFIndex.h
@@ -15,19 +15,23 @@ namespace CC
 //******************************************************************************
 //******************************************************************************
 //******************************************************************************
-// Index used by lock free container classes.
+// Lock Free Index used by CAS based algorithms.
 
 struct LFIndex
 {
+   // Used as a node index.
    int mIndex;
+   // Used to overcome the ABA problem.
    int mCount;
 
+   // Constructor
    LFIndex()
    {
       mIndex = 0;
       mCount = 0;
    }
 
+   // Constructor
    LFIndex(int aIndex, int aCount)
    {
       mIndex = aIndex;
@@ -35,16 +39,19 @@ struct LFIndex
    }
 };
 
+// Operator
 inline bool operator==(const LFIndex& lhs, const LFIndex& rhs)
 {
    return lhs.mIndex == rhs.mIndex && lhs.mCount == rhs.mCount;
 }
 
+// Operator
 inline bool operator!=(const LFIndex& lhs, const LFIndex& rhs)
 {
    return lhs.mIndex != rhs.mIndex && lhs.mCount != rhs.mCount;
 }
 
+// Atomic
 typedef std::atomic<LFIndex> AtomicLFIndex;
 
 //******************************************************************************
