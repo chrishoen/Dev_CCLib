e1289be3784cc890814c3cff21423e920c778501
chris.hoen <chris.hoen@verizon.net>
2016-03-21 16:25:16 -0400

    added ccValueQueue

---
+++ QueueTest/Source/CmdLineExec.cpp
@@ -0,0 +1,91 @@
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <atomic>
+
+#include "ccValueQueue.h"
+
+#include "prnPrint.h"
+#include "CmdLineExec.h"
+
+using namespace std;
+
+//******************************************************************************
+CmdLineExec::CmdLineExec()
+{
+}
+//******************************************************************************
+void CmdLineExec::reset()
+{
+}
+//******************************************************************************
+void CmdLineExec::execute(Ris::CmdLineCmd* aCmd)
+{
+   if(aCmd->isCmd("RESET"  ))  reset();
+   if(aCmd->isCmd("GO1"    ))  executeGo1(aCmd);
+   if(aCmd->isCmd("GO2"    ))  executeGo2(aCmd);
+   if(aCmd->isCmd("GO3"    ))  executeGo3(aCmd);
+   if(aCmd->isCmd("GO4"    ))  executeGo4(aCmd);
+   if(aCmd->isCmd("GO5"    ))  executeGo5(aCmd);
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void CmdLineExec::executeGo1(Ris::CmdLineCmd* aCmd)
+{
+   CC::ValueQueue<int> tQueue;
+   tQueue.initialize(4);
+   
+   int tX,tY;
+   tX=101; tQueue.put(tX);
+   tX=102; tQueue.put(tX);
+   tX=103; tQueue.put(tX);
+   tX=104; tQueue.put(tX);
+
+   Prn::print(0, "Queue %d  %d  %d", tQueue.size(), tQueue.isGet(), tQueue.elementToGet());
+
+   tY = tQueue.get();
+
+   Prn::print(0, "Get   %d", tY);
+   Prn::print(0, "Queue %d  %d  %d", tQueue.size(), tQueue.isGet(), tQueue.elementToGet());
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void CmdLineExec::executeGo2(Ris::CmdLineCmd* aCmd)
+{
+   aCmd->setArgDefault(1,0.99);
+
+   double tC = aCmd->argDouble(1);
+   double tX = (1.0 + tC)/2.0;
+
+   Prn::print(0, "%8.7f %8.7f",tC,tX);
+}
+
+//******************************************************************************
+
+void CmdLineExec::executeGo3(Ris::CmdLineCmd* aCmd)
+{
+}
+
+//******************************************************************************
+
+void CmdLineExec::executeGo4(Ris::CmdLineCmd* aCmd)
+{
+}
+
+//******************************************************************************
+
+void CmdLineExec::executeGo5(Ris::CmdLineCmd* aCmd)
+{
+}
+
