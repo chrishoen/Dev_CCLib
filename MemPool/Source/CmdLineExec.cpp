a18e6d27c6a771b17452928b2fdbdab2a63ffed2
chris.hoen <chris.hoen@verizon.net>
2016-03-29 11:44:16 -0400

    added MemPool project

---
+++ MemPool/Source/CmdLineExec.cpp
@@ -0,0 +1,78 @@
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <atomic>
+
+#include "prnPrint.h"
+#include "someClass1.h"
+#include "CmdLineExec.h"
+
+using namespace std;
+
+//******************************************************************************
+CmdLineExec::CmdLineExec()
+{
+}
+//******************************************************************************
+void CmdLineExec::reset()
+{
+}
+//******************************************************************************
+void CmdLineExec::execute(Ris::CmdLineCmd* aCmd)
+{
+   if(aCmd->isCmd("RESET"  ))  reset();
+   if(aCmd->isCmd("GO1"    ))  executeGo1(aCmd);
+   if(aCmd->isCmd("GO2"    ))  executeGo2(aCmd);
+   if(aCmd->isCmd("GO3"    ))  executeGo3(aCmd);
+   if(aCmd->isCmd("GO4"    ))  executeGo4(aCmd);
+   if(aCmd->isCmd("GO5"    ))  executeGo5(aCmd);
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void CmdLineExec::executeGo1(Ris::CmdLineCmd* aCmd)
+{
+   aCmd->setArgDefault(1,10);
+
+   Some::Class1A tX1;
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void CmdLineExec::executeGo2(Ris::CmdLineCmd* aCmd)
+{
+   aCmd->setArgDefault(1,0.99);
+
+   double tC = aCmd->argDouble(1);
+   double tX = (1.0 + tC)/2.0;
+
+   Prn::print(0, "%8.7f %8.7f",tC,tX);
+}
+
+//******************************************************************************
+
+void CmdLineExec::executeGo3(Ris::CmdLineCmd* aCmd)
+{
+}
+
+//******************************************************************************
+
+void CmdLineExec::executeGo4(Ris::CmdLineCmd* aCmd)
+{
+}
+
+//******************************************************************************
+
+void CmdLineExec::executeGo5(Ris::CmdLineCmd* aCmd)
+{
+}
+
