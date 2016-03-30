feb8ea0c27a13c032e9fb7079251c38d38688444
chris.hoen <chris.hoen@verizon.net>
2016-03-29 23:38:42 -0400

    more MemPool

--- MemPool/Source/someClass1.cpp
+++ MemPool/Source/someClass1.cpp
@@ -33,6 +33,38 @@ void Class1A::method1A()
    Prn::print(0, "Class1A::method1A %d",mCode1A);
 }
 
+//--------------------------------------------------------------------------
+//--------------------------------------------------------------------------
+//--------------------------------------------------------------------------
+// This allocates a block from the block pool and uses placement new
+// to call the class constructor. It is analogous to new.
+
+Class1A* Class1A::allocate()
+{
+   // Block pointer.
+   Class1A* tPointer = 0;
+
+   // Allocate a block from the block pool.
+   tPointer = (Class1A*)CC::getMemPoolBlock(CC::cMemPoolIndex_Class1A);
+
+   // Call the constructor on the allocated block using placement new.
+   new(tPointer)Class1A();
+
+   // Return the pointer to the allocated block.
+   return tPointer;
+}
+
+//--------------------------------------------------------------------------
+//--------------------------------------------------------------------------
+//--------------------------------------------------------------------------
+// This deallocates the object back to the block pool. It does not call
+// a destructor for the object.
+void Class1A::deallocate()
+{
+   // Deallocate the block back to the block pool
+   putMemPoolBlock(this);
+}
+
 //******************************************************************************
 //******************************************************************************
 //******************************************************************************
