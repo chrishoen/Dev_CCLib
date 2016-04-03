964f60f8170a91ce2bee406490629b22966a85f9
chris.hoen <chris.hoen@verizon.net>
2016-04-02 22:56:27 -0400

    cleanup

--- CCLib/Source/ccBlockPoolFreeList.h
+++ CCLib/Source/ccBlockPoolFreeList.h
@@ -13,31 +13,29 @@ Free list block pool class
 
 namespace CC
 {
+
 //******************************************************************************
 //******************************************************************************
 //******************************************************************************
 // This class encapsulates a block pool that is based on a free list paradigm.
 // It is long term and it is not thread safe.
 //
-// It caontains a free list of blocks, which is a b lock array and a stack of
+// It contains a free list of blocks, which is a block array and a stack of
 // indices into the array. When a block is allocated, an index is popped off of
 // the stack. When a block is deallocated, its index is pushed onto the stack.
-   
+//   
 // At initialization, all of the indices are pushed onto the stack. The 
 // pool starts out with a full stack.
 //
 // It allocates memory for the block array and initializes the index stack.
 // It is passed the number of blocks to allocate and the size of the blocks. 
-// Memory for one dummy block is allocated because index zero is reserved to
-// indicate a null block.
 //
-// For aNumBlocks==10 blocks will range 0,1,2,3,4,5,6,7,8,9,10
-// An index of zero is reserved for null index, so block 0 is unused.
-// So usable blocks will range 1,2,3,4,5,6,7,8,9,10
+// For aNumBlocks==10 blocks will range 0,1,2,3,4,5,6,7,8,9
+// So block indices will range 0,1,2,3,4,5,6,7,8,9
 //
 // An index stack is used to manage free list access to the blocks
 // The stack is initialized for a free list by pushing indices onto it.
-// For aAllocate==10 this will push 10,9,8,7,6,5,4,3,2,1
+// For aAllocate==10 this will push 0,1,2,3,4,5,6,7,8,9 onto the stack.
 //
 
 class BlockPoolFreeList : public BlockPoolBase
