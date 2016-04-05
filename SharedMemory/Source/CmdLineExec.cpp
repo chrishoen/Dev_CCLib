9dfd416f746a555b9391eed1ce07305992fda817
chris.hoen <chris.hoen@verizon.net>
2016-04-05 14:51:44 -0400

    add SharedMemory

---
+++ SharedMemory/Source/CmdLineExec.cpp
@@ -0,0 +1,80 @@
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <new>
+
+#include "prnPrint.h"
+#include "ccBlockPoolCentral.h"
+#include "someBlockPoolIndex.h"
+#include "someMyBlockA.h"
+#include "CmdLineExec.h"
+
+using namespace std;
+
+//******************************************************************************
+CmdLineExec::CmdLineExec()
+{
+}
+//******************************************************************************
+void CmdLineExec::reset()
+{
+}
+//******************************************************************************
+
+void CmdLineExec::execute(Ris::CmdLineCmd* aCmd)
+{
+   if(aCmd->isCmd("RESET"  ))  reset();
+   if(aCmd->isCmd("GO1"     ))  executeGo1(aCmd);
+   if(aCmd->isCmd("GO2"     ))  executeGo2(aCmd);
+   if(aCmd->isCmd("GO3"     ))  executeGo3(aCmd);
+   if(aCmd->isCmd("GO4"     ))  executeGo4(aCmd);
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void CmdLineExec::executeGo1(Ris::CmdLineCmd* aCmd)
+{
+   CC::showBlockPool(Some::cBlockPoolIndex_MyBlockA);
+
+   Some::MyBlockA* tC1 = Some::MyBlockA::create(101);
+   tC1->method1();
+
+   CC::showBlockPool(Some::cBlockPoolIndex_MyBlockA);
+
+   tC1->destroy();
+
+   CC::showBlockPool(Some::cBlockPoolIndex_MyBlockA);
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void CmdLineExec::executeGo2(Ris::CmdLineCmd* aCmd)
+{
+   Some::MyBlockA* tC1 = Some::MyBlockA::create(101);
+   Prn::print(0, "%d %d",tC1->mBlockHandle.mPoolIndex,tC1->mBlockHandle.mBlockIndex);
+
+   CC::BlockHandle tBlockHandle = CC::getBlockPoolBlockHandle(tC1);
+   Prn::print(0, "%d %d",tBlockHandle.mPoolIndex,tBlockHandle.mBlockIndex);
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void CmdLineExec::executeGo3(Ris::CmdLineCmd* aCmd)
+{
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void CmdLineExec::executeGo4(Ris::CmdLineCmd* aCmd)
+{
+}
+
