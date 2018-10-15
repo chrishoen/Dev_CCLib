d42884f99a072b163efb783c07bbae0260ef6d58
chrishoen <chris123hoen@gmail.com>
2018-10-15 13:24:11 -0400

    ccSRSWValueQueue removed shared memory code

--- CCLib/Source/ccSRSWValueQueue.h
+++ CCLib/Source/ccSRSWValueQueue.h
@@ -17,75 +17,12 @@ It is thread safe for separate single writer and single reader threads.
 //******************************************************************************
 //******************************************************************************
 
-#include <new>
-#include "ccDefs.h"
-#include "cc_functions.h"
-#include "ccMemoryPtr.h"
-
 //******************************************************************************
 //******************************************************************************
 //******************************************************************************
 
 namespace CC
 {
-//******************************************************************************
-//******************************************************************************
-//******************************************************************************
-// State variables for the stack. These are located in a separate class
-// so that they can be located in external memory.
-
-class SRSWValueQueueState
-{
-public:
-
-   //***************************************************************************
-   //***************************************************************************
-   //***************************************************************************
-   // Class.
-
-   // This returns the number of bytes that an instance of this class
-   // will need to be allocated for it.
-   static int getMemorySize()
-   {
-      return cc_round_upto16(sizeof(SRSWValueQueueState));
-   }
-
-   //***************************************************************************
-   //***************************************************************************
-   //***************************************************************************
-   // Members.
-
-   // Number of elements allocated.
-   int mNumElements;
-   int mPutIndex;
-   int mGetIndex;
-
-   //***************************************************************************
-   //***************************************************************************
-   //***************************************************************************
-   // Methods.
-
-   // Constructor.
-   SRSWValueQueueState()
-   {
-      // All null
-      mNumElements = 0;
-      mPutIndex = 0;
-      mGetIndex = 0;
-   }
-
-   // Initialize.
-   void initialize(int aNumElements,bool aConstructorFlag)
-   {
-      // Do not initialize, if already initialized.
-      if (!aConstructorFlag) return;
-
-      // Allocate for one extra dummy node.
-      mNumElements = aNumElements + 1;
-      mPutIndex = 0;
-      mGetIndex = 0;
-   }
-};
 
 //******************************************************************************
 //******************************************************************************
@@ -99,57 +36,14 @@ public:
    //***************************************************************************
    //***************************************************************************
    //***************************************************************************
-   // Class.
-
-   // This local class calculates and stores the memory sizes needed by the class.
-   class MemorySize
-   {
-   public:
-      // Members.
-      int mStateSize;
-      int mElementArraySize;
-      int mMemorySize;
-
-      // Calculate and store memory sizes.
-      MemorySize(int aNumElements)
-      {
-         mStateSize         = SRSWValueQueueState::getMemorySize();
-         mElementArraySize  = cc_round_upto16(cNewArrayExtraMemory + (aNumElements + 1)*sizeof(Element));
-         mMemorySize = mStateSize + mElementArraySize;
-      }
-   };
-
-   //***************************************************************************
-   //***************************************************************************
-   //***************************************************************************
-   // Class.
-
-   // This returns the number of bytes that an instance of this class
-   // will need to be allocated for it.
-   static int getMemorySize(int aNumElements)
-   {
-      MemorySize tMemorySize(aNumElements);
-      return tMemorySize.mMemorySize;
-   }
-
-   //***************************************************************************
-   //***************************************************************************
-   //***************************************************************************
    // Members.
 
-   // If this flag is false then the memory for this object was created
-   // externally. If it is true then the memory was allocated at 
-   // initialization and must be freed at finalization.
-   bool mOwnMemoryFlag;
+   // Number of elements allocated.
+   int mNumElements;
 
-   // Pointer to memory for which the queue resides. This is either created
-   // externally and passed as an initialization parameter or it is created
-   // on the system heap at initialization.
-   void* mMemory;
-
-   // State variables for the queue. These are located in a separate class
-   // so that they can be located in externale memory.
-   SRSWValueQueueState* mX;
+   // Element access indices.
+   int mPutIndex;
+   int mGetIndex;
 
    // Array of values, storage for the values.
    // Size is NumElements + 1.
@@ -164,12 +58,10 @@ public:
    // Constructor.
    SRSWValueQueue()
    {
-      // All null.
-      mX = 0;
-      mOwnMemoryFlag = false;
-      mMemory = 0;
-
       // All null
+      mNumElements = 0;
+      mPutIndex = 0;
+      mGetIndex = 0;
       mElement = 0;
    }
 
@@ -185,73 +77,16 @@ public:
 
    void initialize(int aNumElements)
    {
-      initialize(aNumElements,true,0);
-   }
-
-   void initialize(int aNumElements,bool aConstructorFlag, void* aMemory)
-   {
-      //************************************************************************
-      //************************************************************************
-      //************************************************************************
-      // Initialize memory.
-
       // Deallocate memory, if any exists.
       finalize();
 
-      // If the instance of this class is not to reside in external memory
-      // then allocate memory for it on the system heap.
-      if (aMemory == 0)
-      {
-         mMemory = malloc(SRSWValueQueue<Element>::getMemorySize(aNumElements));
-         mOwnMemoryFlag = true;
-      }
-      // If the instance of this class is to reside in external memory
-      // then use the memory pointer that was passed in.
-      else
-      {
-         mMemory = aMemory;
-         mOwnMemoryFlag = false;
-      }
-
-      // Calculate memory sizes.
-      MemorySize tMemorySize(aNumElements);
-
-      // Calculate memory addresses.
-      MemoryPtr tMemoryPtr(mMemory);
-
-      char* tStateMemory        = tMemoryPtr.cfetch_add(tMemorySize.mStateSize);
-      char* tElementArrayMemory = tMemoryPtr.cfetch_add(tMemorySize.mElementArraySize);
-
-      // Construct the state.
-      if (aConstructorFlag)
-      {
-         // Call the constructor.
-         mX = new(tStateMemory)SRSWValueQueueState;
-      }
-      else
-      {
-         // The constructor has already been called.
-         mX = (SRSWValueQueueState*)tStateMemory;
-      }
-      // Initialize the state.
-      mX->initialize(aNumElements,aConstructorFlag);
-
-      // Construct the arrays.
-      if (aConstructorFlag)
-      {
-         // Call the constructor.
-         mElement = new(tElementArrayMemory)Element[mX->mNumElements];
-      }
-      else
-      {
-         // The constructor has already been called.
-         mElement = (Element*)tElementArrayMemory;
-      }
-
-      //************************************************************************
-      //************************************************************************
-      //************************************************************************
       // Initialize variables.
+      mPutIndex = 0;
+      mGetIndex = 0;
+
+      // Allocate memory for the array to have an extra element.
+      mNumElements = aNumElements + 1;
+      mElement = new Element[mNumElements];
    }
 
    //***************************************************************************
@@ -261,15 +96,8 @@ public:
 
    void finalize()
    {
-      if (mOwnMemoryFlag)
-      {
-         if (mMemory)
-         {
-            free(mMemory);
-         }
-      }
-      mMemory = 0;
-      mOwnMemoryFlag = false;
+      // Deallocate memory for the array.
+      if (mElement) delete mElement;
    }
 
    //***************************************************************************
@@ -281,8 +109,8 @@ public:
 
    int size()
    {
-      int tSize = mX->mPutIndex - mX->mGetIndex;
-      if (tSize < 0) tSize = mX->mNumElements + tSize;
+      int tSize = mPutIndex - mGetIndex;
+      if (tSize < 0) tSize = mNumElements + tSize;
       return tSize;
    }
 
@@ -305,17 +133,17 @@ public:
    bool tryWrite (Element aElement)
    {
       // Test if the queue is full.
-      int tSize = mX->mPutIndex - mX->mGetIndex;
-      if (tSize < 0) tSize = mX->mNumElements + tSize;
-      if (tSize > mX->mNumElements - 2) return false;
+      int tSize = mPutIndex - mGetIndex;
+      if (tSize < 0) tSize = mNumElements + tSize;
+      if (tSize > mNumElements - 2) return false;
 
       // Local put index
-      int tPutIndex = mX->mPutIndex;
+      int tPutIndex = mPutIndex;
       // Copy the source element into the element at the queue put index
       mElement[tPutIndex] = aElement;
       // Advance the put index
-      if(++tPutIndex == mX->mNumElements) tPutIndex = 0;
-      mX->mPutIndex = tPutIndex;
+      if(++tPutIndex == mNumElements) tPutIndex = 0;
+      mPutIndex = tPutIndex;
       // Done
       return true;
    }
@@ -334,17 +162,17 @@ public:
    bool tryRead(Element* aValue)
    {
       // Test if the queue is empty.
-      int tSize = mX->mPutIndex - mX->mGetIndex;
-      if (tSize < 0) tSize = mX->mNumElements + tSize;
+      int tSize = mPutIndex - mGetIndex;
+      if (tSize < 0) tSize = mNumElements + tSize;
       if (tSize == 0) return false;
 
       // Local index
-      int tGetIndex = mX->mGetIndex;
+      int tGetIndex = mGetIndex;
       // Copy the queue array element at the get index
       *aValue = mElement[tGetIndex];
       // Advance the get index
-      if(++tGetIndex == mX->mNumElements) tGetIndex = 0;
-      mX->mGetIndex = tGetIndex;
+      if(++tGetIndex == mNumElements) tGetIndex = 0;
+      mGetIndex = tGetIndex;
 
       // Done.
       return true;
