a42f3956b6f74e62e96e651907f4e22813e26d4c
chrishoen <chris123hoen@gmail.com>
2022-02-18 11:14:11 -0500

    adding ShareLib

---
+++ ShareLib/smShare.cpp
@@ -0,0 +1,96 @@
+/*==============================================================================
+Description:
+==============================================================================*/
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+#include "stdafx.h"
+
+#include "risSharedMemory.h"
+
+#define  _SMSHARE_CPP_
+#include "smShare.h"
+
+namespace SM
+{
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Shared memory object for the global instance.
+
+Ris::SharedMemory gSharedMemory;
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Initialize, there's no constructor. This is called by the process who
+// first creates the shared memory.
+
+void Share::initialize()
+{
+   mRingBuffer.initialize();
+   mRingWriter.initialize(&mRingBuffer);
+   mRingReader.initialize(&mRingBuffer);
+   mRingTester.reset();
+
+   mRingWriter.mTestFunction = std::bind(
+      &Some::TestTester::doWriteTest, &mRingTester,
+      std::placeholders::_1, std::placeholders::_2);
+
+   mRingReader.mTestFunction = std::bind(
+      &Some::TestTester::doReadTest, &mRingTester,
+      std::placeholders::_1, std::placeholders::_2);
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Show.
+
+void Share::show(int aPF)
+{
+   Prn::print(aPF, "NumAttached      %d", gSharedMemory.getNumAttached());
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Create a shared memory region for the global instance and call placement
+// new with the shared memory address. If this is the first process to access
+// the region (it was created created, not opened) then initialize the global
+// instance. Otherwise, do not initialize it, because the first process
+// already did.
+
+void initializeShare()
+{
+   // Create or open the shared memory.
+   bool tFirstFlag = gSharedMemory.initialize("AAAASHARE2", 4096*32);
+
+   // Create the global instance.
+   gShare = new (gSharedMemory.mMemory) Share;
+
+   // If this the first time that the shared memory was created then
+   // initialize it.
+   if (tFirstFlag)
+   {
+      gShare->initialize();
+   }
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Close the shared memory region for the global instance.
+
+void finalizeShare()
+{
+   // Finalize.
+   gSharedMemory.finalize();
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+}//namespace
