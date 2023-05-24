ecbf70eb7dee5c39ab4d46e34e13054b9a3ad810
chrishoen <chris123hoen@gmail.com>
2023-05-24 17:47:37 -0400

    more SRSWByteQueue

--- SRSWByteQueue/CmdLineExec.cpp
+++ SRSWByteQueue/CmdLineExec.cpp
@@ -5,21 +5,30 @@
 #include <stdio.h>
 #include <limits.h>
 
-#include "risProgramTime.h"
-#include "risBitUtils.h"
-#include "my_functions.h"
+#include "ccSRSWByteQueue.h"
+#include "ccSRSWValueQueue.h"
 #include "CmdLineExec.h"
 
 //******************************************************************************
 //******************************************************************************
 //******************************************************************************
 
+SRSWByteQueue gQueue;
+char gMemory[2000];
+
+CC::SRSWValueQueue<char, 4> gQueue2;
+
 CmdLineExec::CmdLineExec()
 {
+   SRSWByteQueue_initialize(&gQueue, gMemory, 4);
+   gQueue2.reset();
 }
 
 void CmdLineExec::reset()
 {
+   SRSWByteQueue_reset(&gQueue);
+   gQueue2.reset();
+   Prn::print(0, "RESET %d", SRSWByteQueue_size(&gQueue));
 }
 
 //******************************************************************************
@@ -35,6 +44,15 @@ void CmdLineExec::reset()
 void CmdLineExec::execute(Ris::CmdLineCmd* aCmd)
 {
    if(aCmd->isCmd("RESET"))   reset();
+   if (aCmd->isCmd("W22"))    executeWrite(aCmd);
+   if (aCmd->isCmd("R22"))    executeRead(aCmd);
+   if (aCmd->isCmd("SHOW22")) executeShow(aCmd);
+
+   if (aCmd->isCmd("W"))      executeWrite2(aCmd);
+   if (aCmd->isCmd("R"))      executeRead2(aCmd);
+   if (aCmd->isCmd("S"))      executeShow2(aCmd);
+
+
    if (aCmd->isCmd("GO1"))    executeGo1(aCmd);
    if (aCmd->isCmd("GO2"))    executeGo2(aCmd);
    if (aCmd->isCmd("GO3"))    executeGo3(aCmd);
@@ -47,30 +65,107 @@ void CmdLineExec::execute(Ris::CmdLineCmd* aCmd)
 //******************************************************************************
 //******************************************************************************
 
-typedef union PackedS
+void CmdLineExec::executeWrite(Ris::CmdLineCmd* aCmd)
 {
-   unsigned char mUint8;
-   unsigned short mUint16;
-   unsigned int mUint32;
-   int mInt32;
-} PackedT;
+   aCmd->setArgDefault(1, 1);
+   char tChar = aCmd->argInt(1);
+
+   if (SRSWByteQueue_tryWrite(&gQueue, tChar))
+   {
+      Prn::print(0, "WRITE PASS  $$ %d", SRSWByteQueue_size(&gQueue));
+   }
+   else
+   {
+      Prn::print(0, "WRITE FAIL  $$ %d", SRSWByteQueue_size(&gQueue));
+   }
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void CmdLineExec::executeRead(Ris::CmdLineCmd* aCmd)
+{
+   char tChar = 0;
+
+   if (SRSWByteQueue_tryRead(&gQueue, &tChar))
+   {
+      Prn::print(0, "READ PASS   $$ %d", SRSWByteQueue_size(&gQueue), (int)tChar);
+   }
+   else
+   {
+      Prn::print(0, "READ FAIL   $$ %d", SRSWByteQueue_size(&gQueue));
+   }
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void CmdLineExec::executeShow(Ris::CmdLineCmd* aCmd)
+{
+   Prn::print(0, "SIZE  $$ %d", SRSWByteQueue_size(&gQueue));
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void CmdLineExec::executeWrite2(Ris::CmdLineCmd* aCmd)
+{
+   aCmd->setArgDefault(1, 1);
+   char tChar = aCmd->argInt(1);
+
+   if (gQueue2.tryWrite(tChar))
+   {
+      Prn::print(0, "WRITE2 PASS  $$ %d", gQueue2.size());
+   }
+   else
+   {
+      Prn::print(0, "WRITE2 FAIL  $$ %d", gQueue2.size());
+   }
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void CmdLineExec::executeRead2(Ris::CmdLineCmd* aCmd)
+{
+   char tChar = 0;
+
+   if (gQueue2.tryRead(&tChar))
+   {
+      Prn::print(0, "READ2 PASS   $$ %d", gQueue2.size());
+   }
+   else
+   {
+      Prn::print(0, "READ2 FAIL   $$ %d", gQueue2.size());
+   }
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void CmdLineExec::executeShow2(Ris::CmdLineCmd* aCmd)
+{
+   Prn::print(0, "SIZE2  $$ %d", gQueue2.size());
+   Prn::print(0, "PUTI   $$ %d", gQueue2.mPutIndex);
+   Prn::print(0, "GETI   $$ %d", gQueue2.mGetIndex);
+
+   for (int i = 0; i < 5; i++)
+   {
+      Prn::print(0, "%d %d", i, gQueue2.mElement[i]);
+   }
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
 
 void CmdLineExec::executeGo1(Ris::CmdLineCmd* aCmd)
 {
-   PackedT tP; tP.mInt32 = 0;
-   tP.mUint8 = 0xff;
-   Prn::print(0, "mUint8     %x", tP.mInt32);
-
-   tP.mUint8 += 1;
-   Prn::print(0, "mUint8     %x", tP.mInt32);
-
-   tP.mUint8 += 1;
-   Prn::print(0, "mUint8     %x", tP.mInt32);
-
-   tP.mUint8 = 0;
-   tP.mUint8 -= 1;
-
-   Prn::print(0, "mUint8     %x", tP.mInt32);
 }
 
 //******************************************************************************
@@ -116,12 +211,5 @@ void CmdLineExec::executeGo5(Ris::CmdLineCmd* aCmd)
 
 void CmdLineExec::executeGo6(Ris::CmdLineCmd* aCmd)
 {
-   Prn::print(0, "Ris::portableGetBinDir() %s", Ris::portableGetBinDir());
-   Prn::print(0, "Ris::portableGetBinDir() %s", Ris::portableGetBinDir());
-   double tTime = Ris::getProgramTime();
-   unsigned int tTimeMS = Ris::getCpuTimeUIntMS();
-
-   Prn::print(0, "Ris::getProgramTime           %10.6f", tTime);
-   Prn::print(0, "Ris::getCpuTimeUIntMs         %10d", tTimeMS);
 }
 
--- SRSWByteQueue/CmdLineExec.h
+++ SRSWByteQueue/CmdLineExec.h
@@ -53,6 +53,14 @@ public:
    // Methods.
 
    // Execute specific commands.
+   void executeWrite(Ris::CmdLineCmd* aCmd);
+   void executeRead(Ris::CmdLineCmd* aCmd);
+   void executeShow(Ris::CmdLineCmd* aCmd);
+
+   void executeWrite2(Ris::CmdLineCmd* aCmd);
+   void executeRead2(Ris::CmdLineCmd* aCmd);
+   void executeShow2(Ris::CmdLineCmd* aCmd);
+
    void executeGo1(Ris::CmdLineCmd* aCmd);
    void executeGo2(Ris::CmdLineCmd* aCmd);
    void executeGo3(Ris::CmdLineCmd* aCmd);
