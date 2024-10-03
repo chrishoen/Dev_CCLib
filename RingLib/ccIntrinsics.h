35fe2c98b25a8666326013cde3dfb7f3783b4f0c
chrishoen <chris123hoen@gmail.com>
2024-10-03 19:16:27 -0400

    ccIntrinsics

---
+++ RingLib/ccIntrinsics.h
@@ -0,0 +1,25 @@
+#pragma once
+
+/*==============================================================================
+Intrinsics for memory barriers and safe memory accesses.
+==============================================================================*/
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Safe read of 64 bits from shared memory on a 32 bit cpu.
+
+inline long long saferead_i64(long long* aValuePtr)
+{
+   // Loop to do two consecutive reads. If the top 32 bits of both are
+   // the same then done. If they're never the same and the loop finishes
+   // then something is wrong.
+   for (int i = 0; i < 20; i++)
+   {
+      const long long cMask = 0xffffffff00000000;
+      long long volatile tValue1 = *aValuePtr;
+      long long volatile tValue2 = *aValuePtr;
+      if ((tValue1 & cMask) == (tValue2 & cMask)) return tValue1;
+   }
+   return -1;
+}
