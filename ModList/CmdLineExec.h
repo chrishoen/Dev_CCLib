d84cdd38af87c6d9b578e51da1757cba8b4cd131
chrishoen <chris123hoen@gmail.com>
2025-08-30 12:45:12 -0400

    project ModList

---
+++ ModList/CmdLineExec.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "risCmdLineExec.h"
+#include "ccModList.h"
+
+//******************************************************************************
+// specific command line executive
+//******************************************************************************
+
+class CmdLineExec : public Ris::BaseCmdLineExec
+{
+public:
+
+   CC::ModList<int,6> mList;
+   CmdLineExec();
+
+   void reset();
+   void execute(Ris::CmdLineCmd* aCmd);
+
+   void executeGo1(Ris::CmdLineCmd* aCmd);
+   void executeGo2(Ris::CmdLineCmd* aCmd);
+   void executeGo3(Ris::CmdLineCmd* aCmd);
+   void executeGo4(Ris::CmdLineCmd* aCmd);
+   void executeGo5(Ris::CmdLineCmd* aCmd);
+};
+
+//******************************************************************************
+
